9c568cd4b4455797ea182f2c0b66a3ff65deee6c
Nyool <handynugraha29@gmail.com>
2019-08-01 11:18:22 +0700

    initialize v1 api simple tour

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const fs = require('fs')
 const express = require('express');
 const app = express();
+
+// synchronus version to get data simpletour.Jefferson
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 // get url request
 app.get('/', (req, res) => {
@@ -12,6 +16,16 @@ app.get('/', (req, res) => {
 // post url request
 app.post('/user', (req, res) => {
     res.send('You can post to this user url')
+})
+
+// api v1 get simple tours
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        message: 'success',
+        data: {
+            tours
+        }
+    })
 })
 
 const port = 3000;
