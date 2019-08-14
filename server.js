7031dd85dcd93bdb1e6d3153d0c8815563afa5c7
handy <handynugraha29@gmail.com>
2019-08-14 14:48:05 +0700

    video 82 sampai video 86 udah selesai. Disini kita hanya menggunakan localhostnya aja dulu. Kalau udah selesai nanti di upload ke mongo db cloud

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+mongoose
+    .connect(process.env.DATABASE_LOCAL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(con => console.log('connect db localhost success', con.connections));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'a tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'a tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The park camper',
+    price: 4.8
+});
+
+testTour
+    .save()
+    .then(doc => {
+        console.log(doc);
+    })
+    .catch(err => console.log(err));
+
 /**
  * START SERVER
  */
