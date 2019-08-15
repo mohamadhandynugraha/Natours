42e766de864e2337f483aab01b2c16927fbf6699
handy <handynugraha29@gmail.com>
2019-08-15 09:24:13 +0700

    menghapus data sample pada tours-simple.json. Dan juga mengupdate tour model dengan data lebih banyak

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,46 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'a tour must have a name'],
         unique: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'a tour must have durations']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, ' a tour must have group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'a tour must have difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
     },
-    price: {
+    ratingsQuantity: {
         type: Number,
-        required: [true, 'a tour must have a price']
-    }
+        default: 0
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'a tour must have summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'a tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
