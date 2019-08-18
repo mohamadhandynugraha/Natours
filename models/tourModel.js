4b55f461c91d603e57f54f0999e1e85b142a8bb6
handy <handynugraha29@gmail.com>
2019-08-18 09:52:54 +0700

    menambahkan npm library slugify pada tourModel, menambahkan document middleware supaya bisa ada slug.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
             required: [true, 'a tour must have a name'],
             unique: true
         },
+        slug: String,
         duration: {
             type: Number,
             required: [true, 'a tour must have durations']
@@ -64,6 +66,25 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
 });
+
+// DEFINE DOCUMENT MIDDLEWARE IN MONGOOSE, RUNS BEFORE .save() and .create()
+// PRE HOOKS
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.pre('save', function(next) {
+//     console.log('will save the document.');
+//     next();
+// });
+
+// // DEFINE DOCUMENT MIDDLEWARE IN MONGOOSE, RUNS ERFORE .save() and .create()
+// // POST HOOKS
+// tourSchema.post('save', function(doc, next) {
+//     console.log(doc);
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
