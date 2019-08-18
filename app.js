74ef4547c5f00319527fc1a05adef34f790b2a42
handy <handynugraha29@gmail.com>
2019-08-18 16:27:44 +0700

    refactoring our code to more better error handling

--- app.js
+++ app.js
@@ -43,9 +43,24 @@ app.use('/api/v1/users', userRouter);
 // MISALKAN ROUTE /API/TOURS/QWERTY     <-- RUTE INI TIDAK KITA BIKIN. KALAU KITA REQ RUTE TSB AKAN ADA ERROR CANNOT GET ROUTE HTML
 // SOLUSINYA PAKE APP.ALL DARI EXPRESS
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // });
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     });
 });
 
